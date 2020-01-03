2837941d597ee6b1137de9755148055344ffad76
Nitsan Soffair <nitsan.soffair@gmail.com>
2020-01-03 23:23:01 +0200

    ui views

--- js/routes.js
+++ js/routes.js
@@ -3,44 +3,90 @@ const routes = [
         path: '/',
         template: `
             <h1>Widget summary</h1>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
+            <table>
                 <tbody>
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <td>Widget 1</td>
+                        <td><button onclick="router.loadRoute('detail')">Details</button></td>
+                        <td><button>Delete</button></td>
                     </tr>
                     <tr>
-                      <th scope="row">2</th>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                      <td>@fat</td>
+                      <td>Widget 2</td>
+                      <td><button onclick="router.loadRoute('detail')">Details</button></td>
+                      <td><button>Delete</button></td>
                     </tr>
                     <tr>
-                      <th scope="row">3</th>
-                      <td>Larry</td>
-                      <td>the Bird</td>
-                      <td>@twitter</td>
+                      <td>Widget 3</td>
+                      <td><button onclick="router.loadRoute('detail')">Details</button></td>
+                      <td><button>Delete</button></td>
                     </tr>
                   </tbody>
             </table>
+            <button onclick="routes.loadRoute('edit')">Add Widget</button>
         `
     },
     {
         path: '/detail',
-        template: '<h1>Detail</h1>'
+        template: `
+                <h1>Widget 2 Details</h1>
+                <p>Name: Widget 2</p>
+                <p>Number: Two hundred fifty three thousands</p>
+                <h3>Key/Value Pairs</h3>
+                <ol>
+                    <li>first: Teddy</li>
+                    <li>last: Bear</li>
+                    <li>color: brown</li>        
+                </ol>               
+                <button onclick="router.loadRoute('edit')">Edit</button>
+`
     },
     {
         path: '/edit',
-        template: '<h1>Edit</h1>'
+        template: `
+                <h1>Widget 2 Edit</h1>
+                <p>
+                    <span>Name</span>
+                    <input type="text" class="text" value="Widget 2"/>
+                </p>
+                    <p>
+                    <span>Number</span>
+                    <input type="text" class="text" value="253781"/>
+                </p>
+                <h3>Key/Value Pairs</h3>
+                <ol>
+                    <li>
+                        <input type="text" class="text"/>
+                        <input type="text" class="text"/>
+                        <button>+</button>
+                        <button>-</button>
+                    </li>
+                    <li>
+                        <input type="text" class="text"/>
+                        <input type="text" class="text"/>
+                        <button>+</button>
+                        <button>-</button>
+                    </li>
+                    <li>
+                        <input type="text" class="text"/>
+                        <input type="text" class="text"/>
+                        <button>+</button>
+                        <button>-</button>
+                    </li>
+                    <li>
+                        <input type="text" class="text"/>
+                        <input type="text" class="text"/>
+                        <button>+</button>
+                        <button>-</button>
+                    </li>
+                    <li>
+                        <input type="text" class="text"/>
+                        <input type="text" class="text"/>
+                        <button>+</button>
+                        <button>-</button>
+                    </li>
+                </ol>
+                <button>Cancel</button>
+                <button>Save</button>
+`
     }
 ];
