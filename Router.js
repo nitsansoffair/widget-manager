05b4c45fe8db44f863b6e439e3f1b55ea25bf3a9
Nitsan Soffair <nitsan.soffair@gmail.com>
2020-01-03 14:58:12 +0200

    Basic router

--- Router.js
+++ Router.js
@@ -21,5 +21,27 @@ class Router {
                 throw new Error(`the uri ${route.uri} already exists`);
             }
         });
+
+        const route = {
+            uri,
+            callback
+        };
+
+        this.routes.push(route);
+    }
+
+    init(){
+        this.routes.some(({ uri, callback }) => {
+            let regEx = new RegExp(uri.replace(/:[^\s/]+/g, '([\\w-]+)'));
+            let path = window.location.pathname;
+
+            const match = path.match(regEx);
+
+            if(match){
+                let req = { path };
+
+                return callback.call(this, req);
+            }
+        });
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,37 @@
-const router = new RouterClass();
+const router = new Router();
 
-router.get('/', () => {});
+router.get('/', () => {
+    const div = document.getElementById('root');
+    div.innerHTML = "";
 
-router.get('/another-page', () => {});
+    const h1 = document.createElement("h1");
+    const content = document.createTextNode("Index");
+    h1.appendChild(content);
 
-router.init();
+    div.appendChild(h1);
+});
+
+router.get('/detail', () => {
+    const div = document.getElementById('root');
+    div.innerHTML = "";
+
+    const h1 = document.createElement("h1");
+    const content = document.createTextNode("Details");
+    h1.appendChild(content);
+
+    div.appendChild(h1);
+});
+
+router.get('/edit', () => {
+    const div = document.getElementById('root');
+    div.innerHTML = "";
+
+    const h1 = document.createElement("h1");
+    const content = document.createTextNode("Edit");
+    h1.appendChild(content);
+
+    div.appendChild(h1);
+});
+
+window.addEventListener('load', router.init);
+window.addEventListener('hashchange', router.init);
